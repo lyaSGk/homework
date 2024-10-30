7003e6b835d0995c2875789510acc72634013577
lyaSGk <peta38488fhueujr@gmail.com>
2024-10-30 11:35:31 +0200

    Configured pom.xml for project dependencies

---
+++ src/main/java/edu/dnu/fpm/pz/App.java
@@ -0,0 +1,13 @@
+package edu.dnu.fpm.pz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
